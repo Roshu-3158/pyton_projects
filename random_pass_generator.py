bbac6f9be0f07a402a5894f3af6cf71b030fca28
Roshan Dattu Nikam <120217863+Roshu-3158@users.noreply.github.com>
2023-02-17 16:40:18 +0530

    random password generator using python
    here is the project which can select random 4 digit password.

---
+++ random_pass_generator.py
@@ -0,0 +1,6 @@
+import random 
+print("-*-*-*-*-WELCOME-*-*-*-*-")
+print("This is random password generator")
+print()
+random_numbers = random.randint(1000,9999)
+print("Here is your random password: ",random_numbers) 
